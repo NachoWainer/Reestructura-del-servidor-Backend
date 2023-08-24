1f77f75883350d54a8a7670fc59e24dd60129179
Ignacio Wainer <nachowainer@gmail.com>
2023-08-24 20:47:53 -0300

    first-commit

---
+++ src/utils/utils.js
@@ -0,0 +1,6 @@
+
+import bcrypt, { genSaltSync } from "bcrypt"
+export const createHash = password => bcrypt.hashSync(password,genSaltSync(10))
+export const isValidPassword = (user,password)=> bcrypt.compareSync(password,user.password)
+
+
